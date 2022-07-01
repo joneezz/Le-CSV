4920875b96f02fdf41a4ab9fded6d88390bd8b48
Eric J <medsrap@gmail.com>
2022-07-01 00:55:13 -0300

    first commit

---
+++ LerCSV/LerCSV/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Contatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lendo o CSV e mostrando no console!!!");
+            LerCSV();
+            Console.WriteLine("***Fim!!!***");
+            Console.ReadLine();
+            
+
+
+        }
+        static void LerCSV()
+        {
+            var linhas = File.ReadAllLines("Contatos.csv");
+            var lista = new List<Contato>();
+            foreach (var linha in linhas)
+            {
+                var valores = linha.Split(',');
+                var Contato = new Contato(){Nome = valores[0], CPF = valores[1]};
+                lista.Add(Contato);
+            }
+            lista.ForEach(x => Console.WriteLine($"{x.Nome}\t{x.CPF}"));
+        }
+    }
+    public class Contato
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+    }
+}
